594f015b395433e81f8bb937c7f6d807afd55120
Leaf-ym <yiminghd2861@126.com>
2023-02-22 09:16:24 +0800

    feat: 数据JSON化
    Signed-off-by: Leaf-ym <yiminghd2861@126.com>

---
+++ util/src/main/resources/js/学历数据.js
@@ -0,0 +1,20 @@
+let arr = [
+    {
+        label: '无',
+        value: '无'
+    },
+    {
+        label: '学士',
+        value: '学士'
+    },
+    {
+        label: '硕士',
+        value: '硕士'
+    },
+    {
+        label: '博士',
+        value: '博士'
+    }
+];
+
+console.log(JSON.stringify(arr));
