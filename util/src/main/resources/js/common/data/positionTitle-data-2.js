dbe85ea5c48b973e6166fc6a855c8dcc0afc2f9c
Leaf-ym <yiminghd2861@126.com>
2023-04-13 13:38:57 +0800

    fix: 添加第二种职称数据
    Signed-off-by: Leaf-ym <yiminghd2861@126.com>

---
+++ util/src/main/resources/js/common/data/positionTitle-data-2.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-15 15:55:33
+ * @LastEditTime: 2022-02-16 10:25:51
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \meeting-h5-nurse\src\common\zhiwu.js
+ */
+export default [
+  {
+    label: "护士",
+    value: "1",
+  },
+  {
+    label: "主管护师",
+    value: "2",
+  },
+  {
+    label: "副主任护师",
+    value: "3",
+  },
+  {
+    label: "主任护师",
+    value: "4",
+  },
+  {
+    label: "其他",
+    value: "5",
+  },
+  {
+    label: "讲师",
+    value: "6",
+  },
+  {
+    label: "副教授",
+    value: "7",
+  },
+  {
+    label: "教授",
+    value: "8",
+  }
+];
