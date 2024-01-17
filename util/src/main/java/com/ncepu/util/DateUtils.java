177f38172a8af95796146a6b3618f314593a7834
Leaf-ym <yiminghd2861@126.com>
2024-01-17 14:05:05 +0800

    feat：根据出生年月日计算年龄
    Signed-off-by: Leaf-ym <yiminghd2861@126.com>

--- util/src/main/java/com/ncepu/util/DateUtils.java
+++ util/src/main/java/com/ncepu/util/DateUtils.java
@@ -745,6 +745,42 @@ public class DateUtils {
     }
 
     /**
+     * 根据出生年月日计算年龄
+     *
+     * @param birthDay
+     * @return
+     * @throws Exception
+     */
+    public static String getAge(String birthDay) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+            Calendar cal = Calendar.getInstance();
+            if (cal.before(birthDay)) {
+                throw new IllegalArgumentException("The birthDay is before Now.It's unbelievable!");
+            }
+            int yearNow = cal.get(Calendar.YEAR);
+            int monthNow = cal.get(Calendar.MONTH);
+            int dayOfMonthNow = cal.get(Calendar.DAY_OF_MONTH);
+            cal.setTime(sdf.parse(birthDay));
+            int yearBirth = cal.get(Calendar.YEAR);
+            int monthBirth = cal.get(Calendar.MONTH);
+            int dayOfMonthBirth = cal.get(Calendar.DAY_OF_MONTH);
+            int age = yearNow - yearBirth;
+            if (monthNow <= monthBirth) {
+                if (monthNow == monthBirth) {
+                    if (dayOfMonthNow < dayOfMonthBirth) age--;
+                } else {
+                    age--;
+                }
+            }
+            return age + "";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return birthDay;
+        }
+    }
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
